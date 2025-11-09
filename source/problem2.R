f16dd3bbf5131e16b6d12cf8de56f2c196c99d39
amruthab10 <ab6169@cumc.columbia.edu>
2025-11-09 17:47:10 -0500

    created a source folder with two functions that i will be using for probem 2

--- source/problem2.R
+++ source/problem2.R
@@ -1,5 +1,5 @@
 
-#' Problem 2 
+#' Simulation of One sample t-test 
 #'
 #' @param mu true mean
 #' @param n sample size
@@ -18,4 +18,25 @@ sim_ttest <- function(mu, n = 30, sigma = 5) {
     p_value = broom::tidy(tt)$p.value,
     reject  = p_value < 0.05
   )
+}
+
+
+#' Run Many Simulations 
+#'
+#' @param mu_vals 
+#' @param reps 
+#' @param n 
+#' @param sigma 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+many_ttest_sims <- function(mu_vals, reps = 5000, n = 30, sigma = 5) {
+  tidyr::expand_grid(
+    mu_true = mu_vals,
+    iter    = seq_len(reps)) |>
+    dplyr::mutate(
+      res = purrr::map(mu_true, ~ sim_ttest(mu = .x, n = n, sigma = sigma))) |>
+    tidyr::unnest(res)
 }
